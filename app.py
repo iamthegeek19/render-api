2dcc544292febd56aaf43d7355b8c6d9a67e37a5
iamthegeek19 <callmethegeek19@gmail.com>
2024-04-23 13:17:30 +0200

    add user end point

--- app.py
+++ app.py
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/users')
+def vanhu():
+    users = {
+        'name': 'donty',
+        'name1': 'hussein',
+        'name3': 'hameno'
+    }
+    return users
+
 if __name__ == '__main__':
     app.run()
